3bdd57ef5daf9dfd645a8ea9980f68b510932629
Lasitha2001 <lasithamuthukumarana2022@gmail.com>
2025-11-08 03:41:03 +0530

    Initialize project structure with Maven, including wrapper scripts, configuration files, and basic Spring Boot application setup.

---
+++ ayu-backend/src/main/java/com/example/ayu_backend/AyuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ayu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyuBackendApplication.class, args);
+	}
+
+}
---
+++ ayu-backend/src/test/java/com/example/ayu_backend/AyuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ayu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
